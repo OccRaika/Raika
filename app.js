4c2620f424e4917119c2363dbf2d2d38ef044341
OccRaika <eduardo.dac@outlook.com>
2021-12-29 11:06:01 -0600

    raikaH vinculado

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
+
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
