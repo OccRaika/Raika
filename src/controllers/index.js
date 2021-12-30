fc000826e7f45b27ca7ef5023ec42e3522d7dd02
OccRaika <eduardo.dac@outlook.com>
2021-12-30 11:22:08 -0600

    MVC para indez

---
+++ src/controllers/index.js
@@ -0,0 +1,7 @@
+const controller={
+    'index': (req,res,next)=>{
+        res.render('raikaH');
+    }
+};//fin del controlador
+
+module.exports =controller;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var indexController= require("../controllers/index.js")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('raikaH');
-});
+router.get('/', indexController.index);
 
 module.exports = router;
