23526829cba403a4f6c0a6616c4af400b7846297
OccRaika <eduardo.dac@outlook.com>
2021-12-30 11:00:04 -0600

    variables del jscript separados y vinculado

---
+++ public/js/variables/puntosNoCapillas.js
@@ -0,0 +1,44 @@
+/* objeto Json con los puntos verdes que marcan otros puntos que no son capillas */
+export var	puntRel={
+    "type":"Topology","arcs":[],"objects":{
+
+"pD1P1_1":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.20060,19.04134]}]},
+"pD1P1_2":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.19942,19.04079]}]},
+"pD1P1_3":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.20496,19.03985]}]},
+
+"pD2P4_1":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.1946,19.05911]}]},
+
+
+"pD5P1_1":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.19499,19.02575]}]},
+
+"pD5P6_1":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.2016,19.01179]}]},
+"pD5P6_2":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.20187,19.00929]}]},
+
+
+
+"pD7P2_1":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.22284,19.04143]}]},
+"pD7P2_2":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.2185,19.03778]}]},
+
+
+"pD7P3_1":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.2019,19.0371]}]},
+"pD7P3_2":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.20561,19.03726]}]},
+"pD7P3_3":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.20319,19.03456]}]},
+"pD7P3_4":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.20387,19.03454]}]},
+"pD7P3_5":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.209297,19.03663]}]},
+"pD7P3_6":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.20914,19.03651]}]},
+"pD7P3_7":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.20434,19.03234]}]},
+"pD7P3_8":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.21163,19.03542]}]},
+"pD7P3_9":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.2119,19.03316]}]},
+"pD7P3_10":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.20855,19.0301]}]},
+"pD7P3_11":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.2099,19.0278]}]},
+"pD7P3_12":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.2057,19.02956]}]},
+
+"pD7P5_1":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.24677,19.04252]}]},
+
+"pD7P8_1":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.25413,19.02301]}]},
+
+"pD7P10_1":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[-98.28369,18.99651]}]},
+
+
+    }
+    };/* termina objeto Json  puntos rojos rectorias */
