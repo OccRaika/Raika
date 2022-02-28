2c0edb5a50173fc75d5dfddefb1c90006f31fedb
Eduardodac <eduardo.dac@outlook.com>
2022-02-27 19:56:31 -0600

    barra lateral

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,9 @@
 const controller={
     'index': (req,res,next)=>{
         res.render('raikaH');
+    },
+    'sidebar': (req, res, next)=>{
+        res.render('index');
     }
 };//fin del controlador
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ var indexController= require("../controllers/index.js")
 
 /* GET home page. */
 router.get('/', indexController.index);
+router.get('/sidebar', indexController.sidebar)
 
 module.exports = router;
